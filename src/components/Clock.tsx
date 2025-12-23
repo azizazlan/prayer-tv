61ba45112396a818f120868017fcb1cac377a483
azizazlan <azizazlan@gmail.com>
2025-12-23 22:40:15 +0800

    narrow top margin kiosk

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -47,7 +47,7 @@ export default function Clock(props: { now: Accessor<Date> }) {
   return (
     <div
       style={{
-        "margin-top": "3vh",
+        "margin-top": "2vh",
         height: "12vh",
         display: "grid",
         "grid-template-columns": "1fr auto 1fr",
